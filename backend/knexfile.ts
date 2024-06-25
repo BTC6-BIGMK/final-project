688f01ccafe3968e28cf7501838f2f82c7be78b4
Ikuo-Matsutori <135724892+Ikuo-Matsutori@users.noreply.github.com>
2024-06-25 18:17:59 +0900

    add:spot取得&描画
    Co-authored-by: Kohichi-Isogai <Kohichi-Isogai@users.noreply.github.com>
    Co-authored-by: 谷口陽斗 <taniguchi-haruto@users.noreply.github.com>

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -4,39 +4,41 @@ import "dotenv/config";
 
 // Update with your config settings.
 
-export const knexConfig: { [key: string]: Knex.Config } = {
-	development: {
-		client: "postgresql",
-		connection: {
-			database: process.env.POSTGRES_DB,
-			user: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-			port: 5433,
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			directory: "./db/migrations",
-		},
-		seeds: { directory: "./db/seeds" },
-	},
+const knexConfig: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      port: 5433,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: { directory: "./db/seeds" },
+  },
 
-	production: {
-		client: "postgresql",
-		connection: {
-			database: process.env.POSTGRES_DB,
-			user: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			directory: "./db/migrations",
-		},
-		seeds: { directory: "./db/seeds", extension: "ts" },
-	},
+  production: {
+    client: "postgresql",
+    connection: {
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: { directory: "./db/seeds", extension: "ts" },
+  },
 };
+
+export default knexConfig;
