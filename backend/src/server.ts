37523c7f73f37c032f719f6d47a876bd724bec83
YUTA917 <119489307+YUTA917@users.noreply.github.com>
2024-06-25 17:06:13 +0900

    spots_api追加

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,17 @@
 import express, { Express } from "express";
 import { helloRouter } from "./routes/hello";
+import { spotsRouter } from "./routes/spots";
+import { knex } from "knex";
+import { knexConfig } from "../knexfile";
+import type { Knex } from "knex";
+
+const environment = process.env.DATABASE_URL ? "production" : "development";
 
 export const createServer = (): Express => {
-  const app = express();
+	const app = express();
 
-  app.use("/api", helloRouter());
+	app.use("/api", helloRouter());
+	app.use("/api/area-spots", spotsRouter(knex(knexConfig[environment])));
 
-  return app;
+	return app;
 };
