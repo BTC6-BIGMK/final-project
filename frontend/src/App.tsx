12b22cb6a909d65e3783883b73aae9f4098be25d
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-06-26 16:26:30 +0900

    frontend push通知 index.htmlにコード移動

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,8 @@
 import { TutorialPage } from "./pages/TutorialPage";
 import { SpotsMapPage } from "./pages/SpotsMapPage";
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    Notification.requestPermission();
-  }, []);
-
-  navigator.serviceWorker
-    .register(`/service-worker.js`, {
-      scope: "../",
-    })
-    .then((reg) => console.log("サービスワーカーの登録成功", reg.scope))
-    .catch((err) => console.log("サービスワーカーの登録失敗", err));
-
   return (
     <>
       <Routes>
