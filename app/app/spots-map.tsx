ef9e71b496a23e95090bcfacab5bcf95b9356096
Ikuo-Matsutori <135724892+Ikuo-Matsutori@users.noreply.github.com>
2024-06-26 23:00:04 +0900

    add:tutorialページ作成 Co-authored-by: YUTA917 <YUTA917@users.noreply.github.com> Co-authored-by: hiroki-kon <hiroki-kon@users.noreply.github.com> Co-authored-by: 谷口陽斗 <taniguchi-haruto@users.noreply.github.com> Co-authored-by: Kohichi-Isogai <Kohichi-Isogai@users.noreply.github.com>

---
+++ app/app/spots-map.tsx
@@ -0,0 +1,11 @@
+import { Pressable, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
+
+export default function SpotsMapScreen() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text>This is map page!</Text>
+        </View>
+    )
+} 
