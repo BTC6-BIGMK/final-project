d743493d5b6e3f4702a02794bc5a8b2c27b34d15
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-06-27 14:19:46 +0900

    frontend 通知機能設定
    Co-authored-by: YUTA917 <YUTA917@users.noreply.github.com>
    Co-authored-by: Ikuo-Matsutori <Ikuo-Matsutori@users.noreply.github.com>

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,13 +1,69 @@
-import { Slot, Stack } from 'expo-router';
-import { ImageBackground } from 'react-native';
-import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { Slot, Stack } from "expo-router";
+import { ImageBackground } from "react-native";
+
+import React from "react";
+import { StyleSheet, View, Button } from "react-native";
+import * as Notifications from "expo-notifications";
+
+const scheduleNotificationAsync = async () => {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "test title",
+      subtitle: "spot-name",
+      body: "test",
+      sound: "default",
+    },
+    trigger: {
+      seconds: 10,
+    },
+  });
+};
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+const requestPermissionsAsync = async () => {
+  const { granted } = await Notifications.getPermissionsAsync();
+  if (granted) {
+    return;
+  }
+
+  await Notifications.requestPermissionsAsync();
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+});
 
 export default function RootLayout() {
-    return (
-        <>
-            <ImageBackground source={require('@/assets/images/washi.jpeg')} className='flex-1 justify-center z-50' >
-                <Slot />
-            </ImageBackground>
-        </>
-    )
+  React.useEffect(() => {
+    requestPermissionsAsync();
+  });
+  return (
+    <>
+      <ImageBackground
+        source={require("@/assets/images/washi.jpeg")}
+        className="flex-1 justify-center z-50"
+      >
+        <Slot />
+      </ImageBackground>
+      <View style={styles.container}>
+        <Button
+          title="3秒後にプッシュ通知する"
+          onPress={scheduleNotificationAsync}
+        />
+      </View>
+    </>
+  );
 }
