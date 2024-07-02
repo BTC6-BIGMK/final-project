da34b02997beff5172c2f8344ef9eb51ac6b41db
Ikuo-Matsutori <135724892+Ikuo-Matsutori@users.noreply.github.com>
2024-07-02 23:03:17 +0900

    add:arコンテンツ取得api作成 Co-authored-by: 谷口陽斗 <taniguchi-haruto@users.noreply.github.com> Co-authored-by: Kohichi-Isogai <Kohichi-Isogai@users.noreply.github.com> Co-authored-by: YUTA917 <YUTA917@users.noreply.github.com>

--- backend/src/routes/spots.ts
+++ backend/src/routes/spots.ts
@@ -59,7 +59,6 @@ export const spotsRouter = (knex: Knex): Router => {
         description: string;
       }>
     ) => {
-      console.log(req.params.spot_id);
       const spotId = Number(req.params.spot_id);
 
       const contents: {
@@ -76,6 +75,38 @@ export const spotsRouter = (knex: Knex): Router => {
         )
         .where("spot_id", spotId);
       res.send(contents[0]);
+    }
+  );
+
+  router.get(
+    "/:spot_id/contents",
+    async (
+      req: Request<{ spot_id: string }>,
+      res: Response<
+        {
+          lat: number;
+          lng: number;
+          image_url: string;
+          type: string;
+        }[]
+      >
+    ) => {
+      const spotId = Number(req.params.spot_id);
+
+      const contents: {
+        lat: number;
+        lng: number;
+        image_url: string;
+        type: string;
+      }[] = await knex("spots")
+        .join("spot_contents", "spots.spot_id", "=", "spot_contents.spot_id")
+        .select(
+          knex.raw(
+            `ST_y(spots.location) AS lat, ST_x(spots.location) AS lng,spot_contents.image_url,spot_contents.type`
+          )
+        )
+        .where("spots.spot_id", spotId);
+      res.send(contents);
     }
   );
 
