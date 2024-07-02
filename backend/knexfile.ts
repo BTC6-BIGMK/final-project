07b9d992291fa98213ff498ea80911e9e5f7a06e
YUTA917 <119489307+YUTA917@users.noreply.github.com>
2024-07-02 17:34:15 +0900

    knex-file変更

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -5,40 +5,42 @@ import "dotenv/config";
 // Update with your config settings.
 
 const knexConfig: { [key: string]: Knex.Config } = {
-  development: {
-    client: "postgresql",
-    connection: {
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      port: 5433,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./db/migrations",
-    },
-    seeds: { directory: "./db/seeds" },
-  },
+	development: {
+		client: "postgresql",
+		connection: {
+			database: process.env.POSTGRES_DB,
+			user: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			port: 5433,
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			directory: "./db/migrations",
+		},
+		seeds: { directory: "./db/seeds" },
+	},
 
-  production: {
-    client: "postgresql",
-    connection: {
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./db/migrations",
-    },
-    seeds: { directory: "./db/seeds", extension: "ts" },
-  },
+	production: {
+		client: "postgresql",
+		connection: {
+			host: process.env.HOST,
+			port: Number(process.env.PORT),
+			user: process.env.USER,
+			password: process.env.PASSWORD,
+			database: process.env.DATABASE,
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			directory: "./db/migrations",
+		},
+		seeds: { directory: "./db/seeds", extension: "ts" },
+	},
 };
 
 export default knexConfig;
