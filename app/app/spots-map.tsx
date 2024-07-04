5e22868e2e4bb575d19e531c2f5b0ca19f988860
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-07-04 23:04:38 +0900

    spots-map フォアグラウンドでの現在地取得に変更
    Co-authored-by: hiroki-kon <hiroki-kon@users.noreply.github.com>
    Co-authored-by: Ikuo-Matsutori <Ikuo-Matsutori@users.noreply.github.com>

--- app/app/spots-map.tsx
+++ app/app/spots-map.tsx
@@ -3,7 +3,7 @@ import MapView, { Callout, Marker, UrlTile } from "react-native-maps";
 import { useEffect, useState, createRef } from "react";
 import * as Location from "expo-location";
 import axios from "axios";
-import { GeofencingEventType } from "expo-location";
+import { GeofencingEventType, LocationSubscription } from "expo-location";
 import * as TaskManager from "expo-task-manager";
 import * as Notifications from "expo-notifications";
 
@@ -55,9 +55,24 @@ TaskManager.defineTask("GEOFENCE_TASK", ({ data, error }: any) => {
   if (data.eventType === GeofencingEventType.Enter) {
     // TODO: ジオフェンスに入った時の処理を追加
     scheduleNotificationAsync(data.region);
-    console.log("You've entered region:", data.region);
   }
 });
+
+function distance(lat1: number, lng1: number, lat2: number, lng2: number) {
+  const R = Math.PI / 180;
+  lat1 *= R;
+  lng1 *= R;
+  lat2 *= R;
+  lng2 *= R;
+  return (
+    1000 *
+    6371 *
+    Math.acos(
+      Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) +
+        Math.sin(lat1) * Math.sin(lat2)
+    )
+  );
+}
 
 export default function SpotsMapScreen() {
   const [spots, setSpots] = useState<Spot[]>();
@@ -76,40 +91,55 @@ export default function SpotsMapScreen() {
         return;
       }
 
-      const { status: bStatus } =
-        await Location.requestBackgroundPermissionsAsync();
-      console.log(bStatus);
-      if (bStatus !== "granted") {
-        // TODO: 'Permission to access location was denied'
-        return;
-      }
-
       const location = await Location.getCurrentPositionAsync({});
 
       setInitialLocation({
         lat: location.coords.latitude,
         lng: location.coords.longitude,
       });
-
       const response = await axios.get(
+        // `http://192.168.2.110:3000/api/area-spots?lat=${location.coords.latitude}&lng=${location.coords.longitude}&radius=50000`
         `http://localhost:3000/api/area-spots?lat=${location.coords.latitude}&lng=${location.coords.longitude}&radius=500`
       );
       const spotData = response.data as Spot[];
       setSpots(spotData);
-      await Location.startGeofencingAsync(
-        "GEOFENCE_TASK",
-        spotData.map((spot) => ({
-          identifier: `${spot.spot_id},${spot.name}`,
-          latitude: spot.lat,
-          longitude: spot.lng,
-          radius: 10,
-        }))
-      );
     })();
   }, []);
   const { height, width } = Dimensions.get("window");
   const LATITUDE_DELTA = 0.007;
   const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
+
+  useEffect(() => {
+    let locationSubscription: LocationSubscription;
+    (async () => {
+      locationSubscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          distanceInterval: 20,
+        },
+        (loc) => {
+          spots?.forEach((spot) => {
+            const spotDistance = distance(
+              loc.coords.latitude,
+              loc.coords.longitude,
+              spot.lat,
+              spot.lng
+            );
+            if (spotDistance <= 50) {
+              scheduleNotificationAsync({
+                id: spot.spot_id,
+                identifier: `${spot.spot_id},${spot.name}`,
+                latitude: spot.lat,
+                longitude: spot.lng,
+                radius: 50,
+              });
+            }
+          });
+        }
+      );
+    })();
+    return () => locationSubscription.remove();
+  }, [spots]);
 
   return (
     <>
